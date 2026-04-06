526f27719fcc4dd5a915895ebb77557752fa712c
Franco Zacco <francozacco@gmail.com>
2026-04-06 10:02:43 +0200

    ch9

---
+++ constants.py
@@ -0,0 +1,17 @@
+import math
+
+pi = math.pi
+c = 2.997924e8  # m/s
+mu0 = (4 * pi) * (10**-7)  # Tm/A
+e = 1.602e-19  # C
+hbar = 1.054e-34  # Js
+me = 9.109e-31  # kg
+mp = 1.672e-27  # kg
+aB = 5.29e-11  # m
+muB = 5.79e-5  # eV/T
+eps0 = 8.854e-12  # F/m
+k = 8.9877e9  # m/F
+hc = 1240.  # eV
+kB = 1.380e-23  # J/K
+
+J_to_eV = 6.242e18  # eV/J
